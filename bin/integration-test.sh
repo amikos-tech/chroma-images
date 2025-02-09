ace95a84623ba0c40a45093d678f4852af3e0c2a
Trayan Azarov <trayan.azarov@amikos.tech>
2025-02-09 18:53:00 +0200

    feat: Adding proper tests

---
+++ bin/integration-test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+function cleanup {
+  docker stop chroma-server
+}
+
+trap cleanup EXIT
+
+docker run -d --rm --name chroma-server -p ${CHROMA_PORT}:8000 ${FIRST_TAG}
+
+export CHROMA_INTEGRATION_TEST_ONLY=1
+export CHROMA_API_IMPL=chromadb.api.fastapi.FastAPI
+export CHROMA_SERVER_HOST=localhost
+export CHROMA_SERVER_HTTP_PORT=${CHROMA_PORT}
+
+echo testing: python -m pytest "$@"
+cd chroma
+python -m pytest "$@"
+
+docker stop chroma-server
